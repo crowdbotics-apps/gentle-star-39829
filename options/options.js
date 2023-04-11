8fb097700206336dfb2a0319139265bd4af915a4
Crowdbotics <team@crowdbotics.com>
2023-04-11 05:20:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-star-39829",
+  url: "https://gentle-star-39829.botics.co",
+  api: "https://gentle-star-39829.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
